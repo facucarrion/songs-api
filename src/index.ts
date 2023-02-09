85cd1647c7f1948a94bedfd718daa9baaab75880
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-09 10:40:25 -0300

    🪄 linter fixes

--- src/index.ts
+++ src/index.ts
@@ -7,12 +7,12 @@ app.use(express.json()) // Middleware -> req.body to JSON
 const PORT = 3000
 
 app.get('/ping', (_req, res) => {
-  console.log('Someone pinged me!');
+  console.log('Someone pinged me!')
   res.send('pong')
 })
 
 app.use('/api/songs', songRouter)
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`)
 })
