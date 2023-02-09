498d7db21a8bade3b91d2bbd2db663f9e2a98036
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-09 09:27:14 -0300

    ⚒️ installed ts-node-dev and creating routes file

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express'
 
 const app = express()
-app.use(express.json()) // Middleware -> Req to JSON
+app.use(express.json()) // Middleware -> req.body to JSON
 
 const PORT = 3000
 
-app.get('/ping', (_req, res => {
+app.get('/ping', (_req, res) => {
   console.log('Someone pinged me!');
   res.send('pong')
-});
+})
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
