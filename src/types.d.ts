5eecf53dd09b428d578ae3c05dc338256ff28325
Facundo Carrion <facundojcarrion@gmail.com>
2023-02-09 12:26:22 -0300

    🦴 add songWithoutId type and add optional feat and album properties

--- src/types.d.ts
+++ src/types.d.ts
@@ -6,8 +6,9 @@ export interface Song {
   title: string
   duration: number
   genre: Genre
-  feat: string[]
-  album: string
+  feat?: string[]
+  album?: string
 }
 
 export type SongBasics = Pick<Song, 'id' | 'author' | 'title' | 'duration' | 'feat'>
+export type SongWithoutId = Omit<Song, 'id'>
